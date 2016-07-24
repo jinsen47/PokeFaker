2288249a4506486c8ad43f1f4642f483b8868ddc
jinsen47 <jinsen47@gmail.com>
2016-07-24 16:13:24 +0800

    [Feature] Add a search view to main activity
    Signed-off-by: jinsen47 <jinsen47@gmail.com>

--- app/src/main/java/com/github/jinsen47/pokefaker/app/ui/MainActivity.java
+++ app/src/main/java/com/github/jinsen47/pokefaker/app/ui/MainActivity.java
@@ -1,24 +1,30 @@
 package com.github.jinsen47.pokefaker.app.ui;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.DataSetObserver;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Process;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ActionMenuView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.github.jinsen47.pokefaker.R;
+import com.github.jinsen47.pokefaker.app.event.MapPickEvent;
 import com.github.jinsen47.pokefaker.app.service.LocationHolder;
 import com.github.jinsen47.pokefaker.app.service.LocationService;
 import com.github.jinsen47.pokefaker.app.util.StateCheckUtil;
-import com.github.jinsen47.pokefaker.app.event.MapPickEvent;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -27,15 +33,22 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.mypopsy.drawable.SearchArrowDrawable;
+import com.mypopsy.widget.FloatingSearchView;
+import com.mypopsy.widget.internal.ViewUtils;
 
 import org.greenrobot.eventbus.EventBus;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, View.OnClickListener{
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int ICON_SEARCH = 1;
+    private static final int ICON_DRAWER = 2;
+
     private SupportMapFragment mMapFragment;
     private ImageView mZoomIn;
     private ImageView mZoomOut;
+    private FloatingSearchView mSearchView;
 
     private GoogleMap mMap;
     private MarkerOptions mMarkerOpts;
@@ -51,7 +64,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         public void onChanged() {
             LocationHolder holder = LocationHolder.getInstance(MainActivity.this);
             LatLng l = holder.pollLatLng();
-            //Log.d(TAG, "receive location:" + (l == null ? "null" : l.toString()));
             if (l != null && mMarker != null) {
                 mMarker.setPosition(l);
             }
@@ -62,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayUseLogoEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
             getSupportActionBar().setLogo(R.mipmap.ic_launcher);
@@ -71,6 +83,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
         mZoomIn = ((ImageView) findViewById(R.id.icon_zoom_in));
         mZoomOut = ((ImageView) findViewById(R.id.icon_zoom_out));
+        mSearchView = ((FloatingSearchView) findViewById(R.id.view_search));
 
         mMapFragment.getMapAsync(this);
         mZoomIn.setOnClickListener(this);
@@ -94,6 +107,30 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     }
                 })
                 .create();
+        updateNavigationIcon(ICON_SEARCH);
+        mSearchView.showLogo(true);
+        mSearchView.getMenu().findItem(R.id.menu_clear).setVisible(false);
+        mSearchView.setOnSearchListener(mSearchViewListener);
+        mSearchView.setOnIconClickListener(mSearchViewListener);
+        mSearchView.setOnSearchFocusChangedListener(mSearchViewListener);
+        mSearchView.setOnMenuItemClickListener(mSearchViewListener);
+    }
+
+    private void updateNavigationIcon(int itemId) {
+        Context context = mSearchView.getContext();
+        Drawable drawable = null;
+
+        switch(itemId) {
+            case ICON_SEARCH:
+                drawable = new SearchArrowDrawable(context);
+                break;
+            case ICON_DRAWER:
+                drawable = new android.support.v7.graphics.drawable.DrawerArrowDrawable(context);
+                break;
+            }
+        drawable = DrawableCompat.wrap(drawable);
+        DrawableCompat.setTint(drawable, ViewUtils.getThemeAttrColor(this, R.attr.colorControlNormal));
+        mSearchView.setIcon(drawable);
     }
 
     @Override
@@ -183,19 +220,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMarker = googleMap.addMarker(mMarkerOpts);
         fetchSavedLocation();
         if (!(mMarker.getPosition().latitude == 0.0 && mMarker.getPosition().longitude == 0.0)) {
-            CameraPosition cp = new CameraPosition.Builder().target(mMarker.getPosition())
-                    .zoom(15.5f)
-                    .bearing(0)
-                    .tilt(25)
-                    .build();
+            moveCamera(true);
+        }
+    }
+
+    private void moveCamera(boolean animate) {
+        CameraPosition cp = new CameraPosition.Builder().target(mMarker.getPosition())
+                .zoom(15.5f)
+                .bearing(0)
+                .tilt(25)
+                .build();
+        if (animate) {
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp));
+        } else {
+            mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cp));
         }
     }
 
     @Override
     public void onMapClick(LatLng latLng) {
         mMarker.setPosition(latLng);
-        EventBus.getDefault().post(new MapPickEvent(latLng));
+        postLocation(latLng);
+    }
+
+    private void postLocation(LatLng latlng) {
+        EventBus.getDefault().post(new MapPickEvent(latlng));
     }
 
     @Override
@@ -207,6 +256,65 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             case R.id.icon_zoom_out:
                 mMap.animateCamera(CameraUpdateFactory.zoomOut());
                 break;
+        }
+    }
+
+    private static LatLng parseLatlng(String searchText) {
+        if (!searchText.contains(",")) return null;
+        String[] positionArray = searchText.split(",");
+        if (positionArray.length != 2) return null;
+        try {
+            double lat = Double.valueOf(positionArray[0]);
+            double lng = Double.valueOf(positionArray[1]);
+            return new LatLng(lat, lng);
+
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    private SearchViewListener mSearchViewListener = new SearchViewListener();
+
+    private class SearchViewListener implements FloatingSearchView.OnSearchListener, FloatingSearchView.OnIconClickListener, FloatingSearchView.OnSearchFocusChangedListener, ActionMenuView.OnMenuItemClickListener {
+
+        @Override
+        public void onNavigationClick() {
+            mSearchView.setActivated(!mSearchView.isActivated());
+        }
+
+        @Override
+        public void onFocusChanged(final boolean focused) {
+            boolean textEmpty = mSearchView.getText().length() == 0;
+            mSearchView.getMenu().findItem(R.id.menu_clear).setVisible(focused);
+            mSearchView.showLogo(!focused && textEmpty);
+
+            if (focused) {
+                mSearchView.showIcon(true);
+            }
+        }
+
+        @Override
+        public void onSearchAction(CharSequence charSequence) {
+            LatLng result = parseLatlng(charSequence.toString());
+            if (result != null) {
+                mMarker.setPosition(result);
+                moveCamera(true);
+                mSearchView.setActivated(false);
+                postLocation(result);
+            } else {
+                Toast.makeText(MainActivity.this, getString(R.string.invalid_input), Toast.LENGTH_SHORT).show();
+                Log.w(TAG, "invalid input = " + charSequence);
+            }
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menu_clear:
+                    mSearchView.setText("");
+                    break;
+            }
+            return true;
         }
     }
 }
